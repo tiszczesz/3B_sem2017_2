07a010c837717d996b57ee3554a86b775149f4c8
Pawe Szczeszek <pszczesz@gmail.com>
2017-12-09 11:46:16 +0100

    first 1

--- cw6/cw6.js
+++ cw6/cw6.js
@@ -5,13 +5,21 @@ function start(){
 }
 function klik(){
     var dane = document.getElementById("dane").value;
-    alert(dane);
+    var wybor = document.getElementById("wybor").value;
+    var ilosc = parseInt(document.getElementById("ilosc").value);
+    ilosc = !isNaN(ilosc) && ilosc>0 && ilosc<=20 ? ilosc : 5; 
     if(dane.trim()==""){
         document.getElementById("dane").style.border = "solid thin red";
         document.getElementById("wynik").innerHTML = "Błędne dane";
     }else{
         document.getElementById("dane").style.border = "solid thin grey";
         document.getElementById("wynik").innerHTML = "";
+        switch (wybor){
+            case "ul": GenerUL(dane,ilosc); break;
+            case "ol": GenerOL(dane,ilosc); break;
+            case "p": GenerP(dane,ilosc); break;  
+            default :document.getElementById("wynik").innerHTML = "ZONK";
+        }
     }
 }
 
